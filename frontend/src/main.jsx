b5fd4e5e991f7146020bc7f6effbc4ff65ede88d
seoyoon-k <its.yoon.work@gmail.com>
2026-05-06 18:53:36 +0900

    chore: 미션4 코드 마이그레이션

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./css/Reset.css";
+import "./css/Variables.css";
+import "./css/Style.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
