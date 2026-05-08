ed42dcc041a6b95ebad5d81ff13bdbc1c3c60a9b
daeyeong1573 <rlaeodud9983@naver.com>
2026-05-08 18:52:26 +0900

    [feat] 삭제,상세 조회, 업데이트

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ import dotenv from "dotenv";
 import express from "express";
 import { nanoid } from "nanoid";
 import connectDB from "./db.js";
-import { createProduct, getProduct } from "./controllers/productController.js";
+import {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} from "./controllers/productController.js";
 import cors from "cors";
 
 // .env 파일 로드 (반드시 다른 코드보다 먼저!)
@@ -14,8 +20,11 @@ app.use(express.json());
 
 // MongoDB 연결
 connectDB();
-app.get("/product", getProduct);
+app.get("/product", getProducts);
+app.get("/product/:id", getProductById);
 app.post("/product", createProduct);
+app.patch("/product/:id", updateProduct);
+app.delete("/product/:id", deleteProduct);
 
 const PORT = 3000;
 app.listen(PORT, () => {
