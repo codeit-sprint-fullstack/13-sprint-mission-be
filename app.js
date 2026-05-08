588f84d44d6a128f4f20e3b355d07103bfa30a56
singsangsong28 <kuu052888@gmail.com>
2026-05-08 14:39:20 +0900

    스프린트 미션 5 백엔드 완료

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config({ path: resolve(__dirname, ".env") });
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 
 connectDB();
 
