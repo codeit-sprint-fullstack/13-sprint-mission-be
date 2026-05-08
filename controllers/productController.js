f3eb6981f64e4dcc5bcb499e981505f3fa6c1662
daeyeong1573 <rlaeodud9983@naver.com>
2026-05-08 10:13:30 +0900

    [fix] getProduct [] => {} 변경

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,13 +9,12 @@ export const createProduct = async (req, res) => {
   }
 };
 
+export const getProduct = async (req, res) => {
+  try {
+    const productList = await Product.find();
 
-export const getProduct = async (req,res) =>{
-  try{
-    const productList = await Product.find()
-
-    res.status(200).json(productList)
-  }catch(error)[
-    res.status(400).json({message : error.message})
-  ]
-}
+    res.status(200).json(productList);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
