80d7e5111f9504371aecd03d11985376f83c9e99
sgwag6799 <sgwag6799@gmail.com>
2026-05-09 02:30:32 +0900

    feat: cors

--- sprint-mission-5/app.js
+++ sprint-mission-5/app.js
@@ -10,15 +10,17 @@ dotenv.config(); // .env 파일 로드 (맨 먼저!)
 
 const app = express();
 // 개발할 때 (모든 도메인 허용 - 개발 편의상)
-app.use(cors());
+// app.use(cors());
 
 // 배포할 때 (특정 도메인만 허용 - 보안상 좋음)
-// app.use(cors({
-//   origin: [
-//     'http://localhost:5173',           // 개발용 프론트엔드
-//     'https://my-app.vercel.app',       // 배포된 프론트엔드
-//   ],
-// }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // 개발용 프론트엔드
+      "sprint-mission-fe-5.vercel.app", // 배포된 프론트엔드
+    ],
+  }),
+);
 app.use(express.json());
 
 // DB 연결
