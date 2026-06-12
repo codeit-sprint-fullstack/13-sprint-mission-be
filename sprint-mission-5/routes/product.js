f31bf9487a1c21b6bf94a6ea753cae0582197aeb
sgwag6799 <sgwag6799@gmail.com>
2026-06-12 12:24:38 +0900

    chore: package.json scripts 추가 & git 경로,,,

--- sprint-mission-5/routes/product.js
+++
@@ -1,8 +0,0 @@
-// routes/product.js
-// import { Router } from "express";
-// const router = Router();
-
-// router.post("/", asyncHandler(async (req, res) => { ... }));
-// router.get("/:id", asyncHandler(async (req, res) => { ... }));
-
-// export default router;
