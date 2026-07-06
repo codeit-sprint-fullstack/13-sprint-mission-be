092c04cdd3550db03f6bd1833ecaed656766b4cb
apple <apple@appleui-MacBookAir.local>
2026-07-06 10:06:46 +0900

    feat:전역 에러 핸들러 미들웨어 및 404 예외 처리 구현

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+// src/app.ts
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { errorHandler } from "./middlewares/errorHandler";
 
 dotenv.config();
 
@@ -12,5 +14,18 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Panda Market Backend Server is Running!");
 });
+
+// ----------------------------------------------------
+//회원가입, 상품, 댓글 라우터
+// ----------------------------------------------------
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "요청하신 API 라우트를 찾을 수 없습니다.",
+  });
+});
+
+app.use(errorHandler);
 
 export default app;
