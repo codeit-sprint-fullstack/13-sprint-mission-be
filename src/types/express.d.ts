8143f77127962c0e1d7d282c80987a5cc5bfa346
sgwag6799 <sgwag6799@gmail.com>
2026-08-09 12:13:41 +0900

    feat: auth

---
+++ src/types/express.d.ts
@@ -0,0 +1,21 @@
+import { Request } from "express";
+
+// express-jwt(verifyAccessToken/optionalAuthenticate)가 req.auth에 심어주는 JWT payload 모양을 명시
+// 요구사항: declare로 Express 타입 확장
+export interface AuthPayload {
+  userId: number;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      auth?: AuthPayload;
+    }
+  }
+}
+
+// verifyAccessToken 미들웨어를 통과한 뒤에는 req.auth가 항상 채워져 있음을 보장하는 타입
+// (라우터에서 verifyAccessToken을 거친 핸들러에서만 사용)
+export interface AuthenticatedRequest extends Request {
+  auth: AuthPayload;
+}
